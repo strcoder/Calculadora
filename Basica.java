a985afc8b84c726d2b363b4ee99b7b6c45271e18
Antonio Guzman <aguzmanv09@gmail.com>
2019-09-20 13:52:22 -0500

    Agregando la estructura a la clase basica

--- Basica.java
+++ Basica.java
@@ -1,7 +1,44 @@
 /**
- * Basica
+ * Bbsica
  */
 public class Basica {
+    private int a;
+    private int b;
 
+    /*
+    * Metodo constructor
+    */
+    public Basica (int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    /*
+    * Suma los valores de a y b
+    */
+    public void suma() {
+        System.out.println("El resultado de la suma es: " + (this.a + this.b));
+    }
+
+    /*
+    * Resta los valores de a y b
+    */
+    public void resta() {
+        System.out.println("El resultado de la resta es: " + (this.a - this.b));
+    }
+
+    /*
+    * Multiplica los valores de a y b
+    */
+    public void multiplicacion() {
+        System.out.println("El resultado de la multiplicación es: " + (this.a * this.b));
+    }
+
+    /*
+    * Divide los valores de a y b
+    */
+    public void division() {
+        System.out.println("El resultado de la división es: " + ((float)this.a / (float)this.b));
+    }
     
 }
